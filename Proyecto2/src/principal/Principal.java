d6dc1309f7c4ced69f8fa916ab190a263c6b68a1
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-02-12 14:02:20 +0100

    s

--- Proyecto2/src/principal/Principal.java
+++ Proyecto2/src/principal/Principal.java
@@ -3,5 +3,13 @@ package principal;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("SEGUNDO REPOSITORIO");
+		
+		// RAMA1
+		System.out.println("RAMA 1: PRIMER MENSAJE");
+		System.out.println("RAMA 1: SEGUNDO MENSAJE");
+		
+		// RAMA2
+		/*System.out.println("RAMA 2: PRIMER MENSAJE");
+		System.out.println("RAMA 2: SEGUNDO MENSAJE");*/
 	}
 }
