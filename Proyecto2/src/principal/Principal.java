0f36d477e5c8f743d91069cc17529096dfecf3df
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-02-12 14:04:49 +0100

    COMMIT RAMA1 (LOS MENSAJES)

--- Proyecto2/src/principal/Principal.java
+++ Proyecto2/src/principal/Principal.java
@@ -3,5 +3,9 @@ package principal;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("SEGUNDO REPOSITORIO");
+		
+		// RAMA1
+		System.out.println("RAMA1: PRIMER MENSAJE");
+		System.out.println("RAMA1: SEGUNDO MENSAJE");
 	}
 }
